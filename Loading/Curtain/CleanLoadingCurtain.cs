b26cce2dd63dd7078ae4f8c3f106e2f595a21e30
godzzz <g0dzZz1lla@yandex.ru>
2024-01-27 03:05:18 +0400

    Added CleanLoadingCurtain

---
+++ Loading/Curtain/CleanLoadingCurtain.cs
@@ -0,0 +1,23 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// © 2023-2024 Nikolay Melnikov <n.melnikov@depra.org>
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Depra.Loading.Operations;
+
+namespace Depra.Loading.Curtain
+{
+	public readonly struct CleanLoadingCurtain : ILoadingCurtain
+	{
+		async Task ILoadingCurtain.Load(IEnumerable<ILoadingOperation> operations, CancellationToken token)
+		{
+			foreach (var operation in operations)
+			{
+				await operation.Load(_ => { }, token);
+			}
+		}
+
+		void ILoadingCurtain.Unload() { }
+	}
+}
